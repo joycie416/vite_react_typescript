c9e9820c79b27c4c35f94dace7c82efb7425de0e
joycie416 <joycie416@gmail.com>
2025-01-19 22:31:16 +0900

    fix: 홈페이지 에러 발생 버튼 삭제

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,19 +1,10 @@
 import AddForm from "./AddForm";
 import TodoList from "./TodoList";
-import * as Sentry from "@sentry/react";
 
 const HomePage = () => {
-  const errorTestButton = () => {
-    try {
-      throw new Error("커스텀 에러 발생, 센트리 에러 테스트");
-    } catch (error) {
-      Sentry.captureException(error);
-    }
-  };
 
   return (
     <div className="w-full h-screen flex flex-col">
-      <button onClick={errorTestButton}>에러 발생시키기</button>
       <AddForm />
       <TodoList />
     </div>
